3d1571aacc545c6bb05d17b0449c49e0d720d21a
anamilchert <ana.milchert@hotmail.com>
2024-10-16 17:40:51 -0300

    criando controller de empresa

---
+++ android/backend/controllers/empresaControllef.js
@@ -0,0 +1,56 @@
+const Empresa = require('../models/empresa');
+
+exports.criarEmpresa = async (req, res) => {
+  try {
+    const empresa = new Empresa(req.body);
+    await empresa.save();
+    res.status(201).send(empresa);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.obterEmpresas = async (req, res) => {
+  try {
+    const empresas = await Empresa.find();
+    res.status(200).send(empresas);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.obterEmpresaPorId = async (req, res) => {
+  try {
+    const empresa = await Empresa.findById(req.params.id);
+    if (!empresa) {
+      return res.status(404).send();
+    }
+    res.status(200).send(empresa);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+exports.atualizarEmpresa = async (req, res) => {
+  try {
+    const empresa = await Empresa.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!empresa) {
+      return res.status(404).send();
+    }
+    res.status(200).send(empresa);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.deletarEmpresa = async (req, res) => {
+  try {
+    const empresa = await Empresa.findByIdAndDelete(req.params.id);
+    if (!empresa) {
+      return res.status(404).send();
+    }
+    res.status(200).send(empresa);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
