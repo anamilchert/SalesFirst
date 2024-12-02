653f74cc0096dfe796305077736a6493f3f1790c
Luiza Carsten <133827091+a1gumacarsten@users.noreply.github.com>
2024-12-02 19:44:31 -0300

    .

--- android/backend/routes/reunioesRoutes.js
+++ android/backend/routes/reunioesRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const reunioesController = require('../controllers/reunioesController');
 
-router.get('/', getMeetingsByDate);
+
 router.post('/', reunioesController.createMeeting);
 
 module.exports = router;
